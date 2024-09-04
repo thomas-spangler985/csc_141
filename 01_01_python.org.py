85fb6f8503a2518e0429166e3d99a1a2aad84f37
thomas-spangler985 <Thomas_spangler@outlook.com>
2024-09-03 20:43:08 -0400

    Add files via upload

---
+++ 01_01_python.org.py
@@ -0,0 +1,9 @@
+'''
+I really like and appreciate how many reasources given to help new coders are available.
+Not even behind a pay-wall too, all the guides, videos, and FAQ's are just right there when I need them.
+I also think its really cool that it has a whole dedicated section for events going on.
+But what super cool about is that its not just corparate meetings or American focused, it covers everything all over the world!
+Like Right now it has conventions and meet up's listed in places like Brazil, India, the Netherlands and even Estonia!
+I dont even know where that one is on a map! Its just super cool to me how open Python is to helping and promoting others
+
+'''
---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,8 @@
+'''
+Someting I've always wanted to do If I had the skills too was make my own website.
+Not a social media page or website using a boring template from another website.
+But my own website I made with my own five fingers having coded and designed everything.
+I'd like to have a place that I can just have everything I've made be on without worry of it going away.
+All my music, comics, really stupid ideas, art, all of it.
+
+'''
